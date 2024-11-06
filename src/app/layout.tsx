427b549719be5e82f2e7f63e5b8b32d66fea1881
Mornar99 <im002314525@fer.hr>
2024-11-06 22:06:05 +0100

    deployment bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
-import SubpageNavbar from "@/components/SubpageNavbar/SubpageNavbar";
 
 export const metadata: Metadata = {
   title: "Shoenest",
